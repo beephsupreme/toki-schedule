bf0c676ee4e7fe4bc5800006ad6caf980c8dc0e3
Michael Rowsey <michael@Michaels-MacBook-Pro.local>
2025-03-11 15:43:49 -0700

    sync files

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,13 @@ use std::{fs::File, io::Write};
 const URL: &str = "https://www.toki.co.jp/purchasing/TLIHTML.files/sheet001.htm";
 
 fn main() -> std::io::Result<()> {
-    // let mut file = File::create("foo.txt")?;
     //
-    // for i in 1..11 {
-    //     file.write_all(i.to_string().as_bytes())?;
-    //     file.write_all(b"\n")?;
-    // }
     let response = reqwest::blocking::get(URL).unwrap().text().unwrap();
     let html = scraper::Html::parse_document(&response);
     let selector = Selector::parse("td").unwrap();
+
     let mut elements: Vec<String> = Vec::new();
+
     for element in html.select(&selector) {
         let s = element.inner_html().to_string();
         let s = s.replace("\n", "");
