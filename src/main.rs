5803bbed80027bf509efc7052da380bdf1dc75cc
Michael Rowsey <michael@Michaels-MacBook-Pro.local>
2025-03-11 14:07:35 -0700

    sync files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::{fs::File, io::Write};
+
+fn main() -> std::io::Result<()> {
+    let mut file = File::create("foo.txt")?;
+
+    for i in 1..11 {
+        file.write_all(i.to_string().as_bytes())?;
+        file.write_all(b"\n")?;
+    }
+    return Ok(());
 }
